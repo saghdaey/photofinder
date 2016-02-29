4969b56cca8c24719b6f545b2d536ca06d607689
Jeff Jones <jeff.jones1@gmail.com>
2016-02-29 14:59:55 -0500

    added email model

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-var config = require('./config')
+var config = require('./config');
+var mongoose = require('mongoose');
+var path = require('path');
 var sendgrid = require('sendgrid')(config.sendgrid.key);
+var Visitor = require('./db/schema');
 var email = new sendgrid.Email({
   to: "jeff.jones1@gmail.com",
   from: "jeff.jones1@gmail.com",
   subject: "testing",
   text: "Testing Sendgrid"
 });
+
+mongoose.connect('mongodb://localhost:27017/visitors');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+  console.log(Visitor);
+});
+
 
 
 
---
+++ db/schema.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var visitorSchema = new mongoose.Schema ({
+    email: {type: String, required: true},
+    photos: [
+      {url: String}
+    ]
+});
+
+var visitorModel = mongoose.model("Visitor", visitorSchema);
