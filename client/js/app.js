e16d62c24b591402585b4b05d3793b951cd45ad9
sanam <sanam.aghdaey@gmail.com>
2016-03-03 02:30:17 -0500

    added necessary requirements

--- client/js/app.js
+++ client/js/app.js
@@ -1,23 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function(){ 
   //submit event firing to server
-  $('[name = submit]').on('click', function(){
+  $('[name = submit]').on('click', function(event){
+    event.preventDefault();
+    console.log("why"); //doesnt work
     var email = $('[name = email]').val()
+    console.log("hey"); //also doesnt work
+    //check if email is valid
+    //if email invalid add pop up: "the email address you entered is invalid. please try again"
       $.ajax({
         method: "POST",
         url: "/api/fullcontact/visitors",
         data:{email: email}
       }).done(function(response){
-        var photos=response.photos;
-        console.log(photos);
-        if(photos.length<2){
-          console.log("identity unconfirmed. less than 2 photos found.");
-          if(photos.length>0){
-             $('.photo-container').append('<p>One photo found on '+ photos[0].typeName+' </p>');
-            }
-          $('.photo-container').append('<p>Identity not confirmed, condition of 2 photos not met.</p>');
-          }
-         
-        else{
+        console.log(response);
+        if(response.photos==undefined){
+          console.log("identity not confirmed. no data found by web scraping");
+          $('.photo-container').append('<p>Identity not confirmed, no photos found.</p>');
+        }
+        else{ //photo was found - update webpage 
+          var photos=response.photos;
           console.log('identity confirmed');
           console.log(photos);
           $.each(photos,function(index,photo){
